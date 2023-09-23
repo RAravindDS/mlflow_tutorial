4ed750a6a89f60766c8a58c2631d64f0f85356a1
Aravindan R <85152278+RAravindDS@users.noreply.github.com>
2023-09-23 06:13:35 +0000

    added mlflow for dagshub

---
+++ testing.py
@@ -0,0 +1,9 @@
+MLFLOW_TRACKING_URI=https://dagshub.com/RAravindDS/mlflow_tutorial.mlflow \
+MLFLOW_TRACKING_USERNAME=RAravindDS \
+MLFLOW_TRACKING_PASSWORD=050d1aaaa48c4c9841a6cf89225729cda3914307 \
+python script.py
+
+
+export MLFLOW_TRACKING_URI=https://dagshub.com/RAravindDS/mlflow_tutorial.mlflow
+export MLFLOW_TRACKING_USERNAME=RAravindDS
+export MLFLOW_TRACKING_PASSWORD=050d1aaaa48c4c9841a6cf89225729cda3914307
